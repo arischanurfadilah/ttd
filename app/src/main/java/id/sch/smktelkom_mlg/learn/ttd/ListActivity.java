89428f95c15f502d9b9200979fa8167acf549cd5
Arischa Nur Fadilah <ariskanf12@gmail.com>
2017-08-01 10:40:13 +0700

    commit1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/ttd/ListActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.learn.ttd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/ttd/MainActivity.java
@@ -0,0 +1,33 @@
+package id.sch.smktelkom_mlg.learn.ttd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnViewProducts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Buttons
+        btnViewProducts = (Button) findViewById(R.id.btnViewProducts);
+
+        // view products click event
+        btnViewProducts.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Launching All products Activity
+                Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
